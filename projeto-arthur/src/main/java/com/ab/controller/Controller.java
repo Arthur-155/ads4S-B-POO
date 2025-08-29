072b37cece91ddd98ed9110fad5a607ccc07d93e
arthur bergamo <arthur_bergamo@hotmail.com>
2025-08-28 22:14:04 -0300

    foi adicionado o controller, service, e o repository

--- projeto-arthur/src/main/java/com/ab/controller/Controller.java
+++ projeto-arthur/src/main/java/com/ab/controller/Controller.java
@@ -1,5 +1,18 @@
 package com.ab.controller;
 
+import com.ab.model.Model;
+import com.ab.service.FestaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/festa")
 public class Controller {
-    
+
+    private final FestaService service;
+
+    public Controller (FestaService service){
+        this.service = service;
+    }
+
 }
---
+++ projeto-arthur/src/main/java/com/ab/service/FestaService.java
@@ -0,0 +1,19 @@
+package com.ab.service;
+
+import com.ab.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FestaService {
+
+    private final Controller controller;
+
+    public FestaService(Controller controller){
+        this.controller = controller;
+    }
+
+    public Model pessoaDaFesta (Model pessoa){
+        return 
+    }
+}
